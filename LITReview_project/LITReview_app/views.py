875b2e775ddcda5d58b4ec5dc914821831a9449a
maticha84 <maticha84@gmail.com>
2021-09-21 11:14:52 +0200

    creation : - base.html - index.html - database sqlite3 - models.py - views.py start to custom website - some models are create but no abject in the database yet

--- LITReview_project/LITReview_app/views.py
+++ LITReview_project/LITReview_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    message = "salut la foule"
+    context = {
+        'message': message
+    }
+    return render(request, 'LITReview_app/index.html', context)
