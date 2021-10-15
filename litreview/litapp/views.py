33756b22316e51e198062ace37bdd596d9acc2ad
maticha84 <maticha84@gmail.com>
2021-10-15 16:05:27 +0200

    Site fonctionel en local. TODO rédaction du README

--- litreview/litapp/views.py
+++ litreview/litapp/views.py
@@ -6,13 +6,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.password_validation import validate_password
 from itertools import chain
 from django.db.models.fields import CharField
-from django.db.models import Value
+from django.db.models import Value, Q
 
 from .models import User, Ticket, Review, UserFollows
 from .forms import TicketForm, ReviewForm
 
 
 def index(request):
+    """
+    Diplay the page of the root server path
+    """
     title = "Bienvenue sur le serveur LitReview - cliquez sur l'application qui vous intéresse"
     context = {
         'message': title
@@ -21,6 +24,10 @@ def index(request):
 
 
 def login_view(request):
+    """
+    Display login page
+    if request method is POST, connexion test
+    """
     title = "Page de login"
     login_echec = ""
 
@@ -43,11 +50,18 @@ def login_view(request):
 
 
 def logout_view(request):
+    """
+    logout view
+    """
     logout(request)
     return redirect('login')
 
 
 def new_account(request):
+    """
+    diplay creation user page
+    if request method is POST, testing creation new user
+    """
     title = "Création d'un compte"
     success = ""
 
@@ -95,9 +109,10 @@ def new_account(request):
 
 @login_required(login_url='login')
 def home_view(request):
+    """
+    Diplay flux page. It's the default page when user is login
+    """
     title_page = "Flux"
-    #tickets = Ticket.objects.order_by('-time_created')
-    #reviews = Review.objects.order_by('-time_created')
 
     reviews = get_users_viewable_reviews(request.user)
     # returns queryset of reviews
@@ -119,12 +134,18 @@ def home_view(request):
         'tickets': tickets,
         'reviews': reviews,
         'posts': posts,
+        'list_id': list_id,
+        'list_all': list_all,
     }
 
     return render(request, 'litapp/home.html', context)
 
 
 def get_users_viewable_tickets(user):
+    """
+    get tickets from user and the users whose followed by user
+    return : QuerySet of Ticket
+    """
     list_user = [user]
 
     users_followed = UserFollows.objects.filter(user=user)
@@ -138,19 +159,37 @@ def get_users_viewable_tickets(user):
 
 
 def get_users_viewable_reviews(user):
+    """
+    get review from user and the users whose followed by user
+    if a user not followed by the login user comment a ticket write by the user connected, it will be
+    display to.
+    return : QuerySet of Review
+    """
     list_user = [user]
-
+    list_id = []
     users_followed = UserFollows.objects.filter(user=user)
     for u in users_followed:
         user_follow = User.objects.filter(followed_by=u)
         for us in user_follow:
             list_user.append(us)
-    reviews = Review.objects.filter(user__in=list_user)
+
+    reviews = Review.objects.all()
+    for review in reviews:
+        if review.ticket.user == user:
+            list_id.append(review.id)
+
+    reviews = Review.objects.filter(Q(user__in=list_user) | Q(id__in=list_id))
     return reviews
 
 
 @login_required(login_url='login')
 def review_create(request):
+    """
+    Display review without ticket creation
+    Method post :
+    first, creation of Ticket
+    second : if Ticket is create, creation of Review
+    """
     title_page = "Créer une critique"
 
     if request.method == 'POST':
@@ -200,6 +239,10 @@ def review_create(request):
 
 @login_required(login_url='login')
 def review_response(request, ticket_id):
+    """
+    Display review creation with existing ticket
+    Method post : response of Ticket
+    """
     ticket = get_object_or_404(Ticket, pk=ticket_id)
     title_page = f"Vous répondez au ticket {ticket.title}"
 
@@ -229,6 +272,9 @@ def review_response(request, ticket_id):
 
 @login_required(login_url='login')
 def review_change(request, review_id):
+    """
+    Display modification review view
+    """
     review = get_object_or_404(Review, pk=review_id)
 
     title_page = f"Vous modifiez la critique {review.id}"
@@ -263,6 +309,9 @@ def review_change(request, review_id):
 
 @login_required(login_url='login')
 def review_delete(request, review_id):
+    """
+    Delete a review
+    """
     review = get_object_or_404(Review, pk=review_id)
     review.delete()
     return redirect('posts')
@@ -270,6 +319,9 @@ def review_delete(request, review_id):
 
 @login_required(login_url='login')
 def ticket_create(request):
+    """
+    Display creation Ticket view
+    """
     title_page = "Créer un ticket"
 
     if request.method == 'POST':
@@ -295,6 +347,9 @@ def ticket_create(request):
 
 @login_required(login_url='login')
 def ticket_change(request, ticket_id):
+    """
+    Diplay modification Ticket view
+    """
     title_page = f"Modification du ticket {ticket_id}"
     ticket = get_object_or_404(Ticket, pk=ticket_id)
     ticket_data = {
@@ -329,6 +384,9 @@ def ticket_change(request, ticket_id):
 
 @login_required(login_url='login')
 def ticket_delete(request, ticket_id):
+    """
+    Delete Ticket
+    """
     ticket = get_object_or_404(Ticket, pk=ticket_id)
     ticket.delete()
     return redirect('posts')
@@ -336,11 +394,14 @@ def ticket_delete(request, ticket_id):
 
 @login_required(login_url='login')
 def posts_view(request):
+    """
+    Display Posts by time_created from the user
+    """
     title_page = "Vos posts"
-    reviews = Review.objects.order_by('-time_created')
+    reviews = Review.objects.filter(user=request.user)
     reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
 
-    tickets = Ticket.objects.order_by('-time_created')
+    tickets = Ticket.objects.filter(user=request.user)
     tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
 
     # combine and sort the two types of posts
@@ -362,8 +423,13 @@ def posts_view(request):
 
 @login_required(login_url='login')
 def follow_view(request):
+    """
+    Diplay follow view
+    method POST : if username is valid and not followed by the user, add the following user
+    """
     page_title = 'Abonnements'
     success = ''
+    echec = ''
     list_followed = []
     list_following = []
 
@@ -373,16 +439,20 @@ def follow_view(request):
     following = UserFollows.objects.filter(followed_user=request.user)
     for u in following:
         list_following.append(u)
+
+    context = {
+        'message': page_title,
+        'echec': echec,
+        'success': success,
+        'followed_by': list_followed,
+        'following': list_following,
+    }
+
     if request.method == 'POST':
         follow_user = request.POST.get('follow_user')
         if follow_user == request.user.username:
             echec = "Vous ne pouvez pas vous abonner à vous-même"
-            context = {
-                'message': page_title,
-                'echec': echec,
-                'followed_by': list_followed,
-                'following': list_following,
-            }
+            context['echec'] = echec
             return render(request, 'litapp/follow.html', context)
         existing_user = User.objects.filter(username=follow_user)
 
@@ -393,37 +463,24 @@ def follow_view(request):
                 user_follows.save()
                 list_followed.append(user_follows)
                 success = f"Vous êtes abonnés à {existing_user.username}"
+                context['success'] = success
             except:
                 echec = "Vous êtes déjà abonnés à cet utilisateur"
-                context = {
-                    'message': page_title,
-                    'echec': echec,
-                    'followed_by': list_followed,
-                    'following': list_following,
-                }
+                context['echec'] = echec
                 return render(request, 'litapp/follow.html', context)
         else:
             echec = "Le nom de l'utilisateur demandé n'existe pas"
-            context = {
-                'message': page_title,
-                'echec': echec,
-                'followed_by': list_followed,
-                'following': list_following,
-            }
+            context['echec'] = echec
             return render(request, 'litapp/follow.html', context)
 
-    context = {
-        'message': page_title,
-        'success': success,
-        'followed_by': list_followed,
-        'following': list_following,
-
-    }
     return render(request, 'litapp/follow.html', context)
 
 
 @login_required(login_url='login')
 def follow_delete(request, followed_by_id, following_id):
+    """
+    Delete following.
+    """
     followed_by = UserFollows.objects.filter(user_id=following_id, followed_user_id=followed_by_id)
     if followed_by:
         followed_by = UserFollows.objects.get(user_id=following_id, followed_user_id=followed_by_id)
